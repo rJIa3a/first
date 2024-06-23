7b75a49e47100769e8764eed60ab40d88f3802e0
rJIa3a <rJIa3a@gmail.com>
2024-06-23 10:26:38 +0300

    Update api_fields.py

--- api_fields.py
+++ api_fields.py
@@ -2,23 +2,13 @@ hit_field_list = [
     "ym:pv:date",
     "ym:pv:dateTime",
     "ym:pv:URL",
-    "ym:pv:deviceCategory",
-    "ym:pv:operatingSystemRoot",
     "ym:pv:clientID",
-    "ym:pv:browser",
-    "ym:pv:lastTrafficSource",
 ]
 
 visit_field_list = [
     "ym:s:date",
     "ym:s:dateTime",
-    "ym:s:pageViews",
-    "ym:s:visitDuration",
     "ym:s:startURL",
-    "ym:s:operatingSystemRoot",
     "ym:s:clientID",
-    "ym:s:browser",
-    "ym:s:browserEngine",
-    "ym:s:lastTrafficSource",
     "ym:s:goalsID"
 ]
