10ea33c9a7db7a4e6254ceb05120fba3a41c20d6
rJIa3a <rJIa3a@gmail.com>
2024-06-23 05:20:57 +0300

    Update api_fields.py

--- api_fields.py
+++ api_fields.py
@@ -7,9 +7,6 @@ hit_field_list = [
     "ym:pv:clientID",
     "ym:pv:browser",
     "ym:pv:lastTrafficSource",
-    "ym:pv:goalsID",
-    "ym:pv:parsedParamsKey1",
-    "ym:pv:parsedParamsKey2"
 ]
 
 visit_field_list = [
@@ -18,20 +15,10 @@ visit_field_list = [
     "ym:s:pageViews",
     "ym:s:visitDuration",
     "ym:s:startURL",
-    "ym:s:deviceCategory",
     "ym:s:operatingSystemRoot",
     "ym:s:clientID",
     "ym:s:browser",
     "ym:s:browserEngine",
-    "ym:s:screenOrientation",
-    "ym:s:screenWidth",
-    "ym:s:screenHeight",
-    "ym:s:physicalScreenWidth",
-    "ym:s:physicalScreenHeight",
     "ym:s:lastTrafficSource",
-    "ym:s:purchaseRevenue",
-    "ym:s:purchaseID",
-    "ym:s:bounce",
-    "ym:s:isNewUser",
     "ym:s:goalsID"
 ]
